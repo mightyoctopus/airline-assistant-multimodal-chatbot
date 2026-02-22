96889b2e01334c1672bc4f31808b28b1fdcf8f80
mhhong <mh.hong@proton.me>
2026-02-22 16:57:50 +0900

    Clean the script by removing unnecessary lines of code

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from interfaces.gradio_ui import GradioUI
 if __name__ == "__main__":
     app = GradioUI()
     port = int(os.environ.get("PORT", 8080))
+    print("ENVIRON: ", os.environ)
     app.ui.launch(
         server_name="0.0.0.0",
         server_port=port,
         allowed_paths=["/tmp"]
     )
-
 
 
 
